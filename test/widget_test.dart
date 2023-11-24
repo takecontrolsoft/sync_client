55331d427a9ae81d47c64c73a9ca23d223d807d8
desistefanova <dst.stefanova@gmail.com>
2023-11-24 21:20:37 +0200

    test coverage

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,7 @@ void main() {
 
     // Verify that our counter starts at 0.
     //expect(find.text('0'), findsOneWidget);
-    //expect(find.text('1'), findsNothing);
+    expect(find.text('Directory:'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
     //await tester.tap(find.byIcon(Icons.add));
