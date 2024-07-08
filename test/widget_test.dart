612e711fcdd31887b72383af54df999b8bd82e63
Desislava Stefaanova <dst.stefanova@gmail.com>
2024-07-09 01:20:17 +0300

    commented tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:sync_client/main.dart';
+//import 'package:sync_client/main.dart';
 
 void main() {
   group('Widgets tests', () {
