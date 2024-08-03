e34be3269c729c58109de8a663fc4691a187a6c2
Desislava Stefaanova <dst.stefanova@gmail.com>
2024-08-03 12:21:52 +0300

    unused reference

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:sync_client/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
